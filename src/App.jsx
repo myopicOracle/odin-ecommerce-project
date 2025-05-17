ee24bdab4a29701468a22ab0176caaa362680b4a
myopicOracle <184602237+myopicOracle@users.noreply.github.com>
2025-05-16 23:09:12 -0700

    chore: add directories for pages, context, data, utils

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react"
-// import './App.css'
 import Header from "./components/Header"
 import { CartContext } from "./components/CartContext.jsx"
 import { ProductContext } from './components/ProductContext.jsx'
@@ -7,7 +6,6 @@ import Products from "./components/Products.jsx"
 import productData from './components/productData.jsx'
 
 // console.log("App.jsx -- TOP LEVEL Check, cart count: ", cartCount);
-
 
 function App() {
     const [cartCount, setCartCount] = useState(0)
@@ -19,25 +17,16 @@ function App() {
     return (
         <div className="rootWrapper flex flex-col justify-center items-center h-screen w-screen">
             
-            {/* <div id="outerWrapper" className="outerWrapper h-full w-clamp-0 sm:w-clamp-sm"> */}
             <div id="outerWrapper" className="outerWrapper h-full w-full sm:w-[90%]">
                 
                 <CartContext.Provider value={{ cartCount, setCartCount }} >
                     <Header />
                 </CartContext.Provider>
 
-                {/* <Body> */}
-
                 <ProductContext.Provider value={{ productsArray, setProductsArray }} >
                     <Products />
                 </ProductContext.Provider>
 
-                    {/* <Cart /> */}
-                    {/* <Checkout /> */}
-                    {/* <OrderConfirmation /> */}
-                {/* </Body> */}
-
-                {/* <Footer /> */}
             </div>
         </div>
     )
